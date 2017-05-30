218fa457f51d4f507d11a721fe361c434e6ced7a
blacktutle <1063819523@qq.com>
2017-05-31 00:11:17 +0800

    Create notfound.php

---
+++ CET/version2.0/notfound.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no">
+		<title>CET四六级成绩查询</title>
+		<style type="text/css">
+            p{
+                text-align: center;
+            	margin-top: 50%;
+                font-size: 30px;
+                color: #00A2E9;
+            }
+            #div1{
+                background-image: url("./img/index_bg.png");
+                height: 100%;
+                width: 100%;
+                background-size: cover;
+                position:fixed;
+                top:0;
+                left:0;
+                bottom:0;
+                right:0;
+                z-index:-1;
+            }
+            .button{
+                outline: none;
+                margin-top: 100px;
+                background-color: transparent;
+              	border-radius: 14px;
+    			border: 2px solid #00A2E9;
+               	width: 100px;
+    			padding: 5px 10px;
+                font-size: 19px;
+                color: #00A2E9;
+    			text-align: center;
+    			text-decoration: none;
+    			display: inline-block;
+                position: relative;
+                left: 50%;
+                margin-left: -50px;
+            }
+            .bottom{
+            	margin-top: 100px;
+                color: #00A2E9;
+                text-align: center;
+                font-size: 13px;
+            }
+		</style>
+	</head>
+	<body>
+        <div id="div1"></div>
+        <p>糟了，找不到该同学</p>
+        <button class="button" onclick="window.location.href='./index.php'">继 续</button>
+		<div class="bottom">Powered by 小耶科技</div>
+	</body>
+</html>
